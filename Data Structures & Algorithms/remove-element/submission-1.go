9df6f3e74b163f41644770befc52a6520bd09769
Ethan <100881105+Ethanmurray3@users.noreply.github.com>
2026-04-04 13:06:06 -0600

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.go
@@ -0,0 +1,13 @@
+func removeElement(nums []int, val int) int {
+	i := 0
+	n := len(nums)
+	for i < n {
+    	if nums[i] == val {
+        	nums[i] = nums[n-1]
+        	n--
+    	} else {
+        	i++
+    	}
+	}
+	return i
+}
